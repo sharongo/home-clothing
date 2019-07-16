09cefd17e66293029b9d478b481c88a905321490
sharon gold <sharongo113@gmail.com>
2019-07-16 22:39:58 +0200

    change stucture folder

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './homepage.scss'
+import Directory from '../../components/directory/directory'
+import MenuItem from '../../components/menu-item/menu-item'
+
+
+const homepage = () => {
+    return (
+        <div className='homepage'>
+               <Directory/>
+        </div>
+    )
+}
+
+
+
+export default homepage
+
+
