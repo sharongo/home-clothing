fb5401aee1867682ce01924dae336a69d7abb7b7
sharon gold <sharongo113@gmail.com>
2019-07-17 12:07:22 +0200

    add routing

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './homepage.scss'
 import Directory from '../../components/directory/directory'
-import MenuItem from '../../components/menu-item/menu-item'
+
 
 
 const homepage = () => {
