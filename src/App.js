ee2484509a7b6416204bbe03355ac7c51d1a19b8
sharon gold <sharongo113@gmail.com>
2019-07-18 11:00:09 +0200

    add signin form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shopPage'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/header'
+
 
 
 
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
