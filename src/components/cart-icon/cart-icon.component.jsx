2108bef214b705c0f10afde1917c6180c9ee5eed
sharon gold <sharongo113@gmail.com>
2019-07-20 12:18:56 +0200

    add cart dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
 
 
-const CartIcon = props => {
+const CartIcon = ({ toggleCartHidden }) => {
     return (
-        <div className='cart-icon' >
+        <div className='cart-icon' onClick={toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
             <span className='item-count'>0</span>
         </div>
     )
 }
 
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+  });
 
 
-export default CartIcon
+
+export default connect(null, mapDispatchToProps)(CartIcon)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
 
     const signOut = () => {
 
@@ -33,14 +33,16 @@ const Header = ({ currentUser }) => {
                             SIGN IN
                         </Link>
                     )}
-                    <CartIcon/>
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropdown />}
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header)
