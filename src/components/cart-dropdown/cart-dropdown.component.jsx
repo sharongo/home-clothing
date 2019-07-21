b7521541c6ff8c666f1e9ac826b706cff697267a
sharon gold <sharongo113@gmail.com>
2019-07-21 10:58:10 +0200

    cart icon counter

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.styles.scss';
 
 
@@ -11,18 +11,18 @@ const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'>
-            {cartItems.map(cartItem => (
-                <CartItem key={cartItem.id} item={cartItem} />
-            ))}
+                {cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
             </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
-});
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+  });
 
 
 
